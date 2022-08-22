7bd52bc6ed0a7c31ed78a1a5ef85a24f76630272
Haodong Li <haodong_li@berkeley.edu>
2022-08-22 10:03:25 -0700

    add SL for cate

---
+++ R/fit_sl3.R
@@ -0,0 +1,141 @@
+library(sl3)
+
+# -- sl setup
+lrnr_lm <- Lrnr_glm$new()
+lrnr_lasso <- Lrnr_glmnet$new()
+lrnr_xgb <- Lrnr_xgboost$new()
+lrnr_gam <- Lrnr_gam$new()
+lrnr_earth <- Lrnr_earth$new()
+lrnr_ranger <- Lrnr_ranger$new()
+
+lrnr_stack <- make_learner("Stack",
+                           lrnr_lm,
+                           lrnr_xgb)
+
+
+ls_metalearner <- make_learner(Lrnr_nnls)
+lb_metalearner <- make_learner(Lrnr_solnp,
+                               learner_function = metalearner_logistic_binomial,
+                               loss_function = loss_loglik_binomial)
+
+# -- sl modeling function
+fitSL <- function(df, df_train = df){
+  
+  folds <- origami::make_folds(strata_ids = df_train$A)
+  
+  # setup sl3
+  task_Q <- sl3::make_sl3_Task(
+    data = df_train,
+    covariates = setdiff(names(df_train), c('Y')),
+    outcome = 'Y',
+    folds = folds
+  )
+  
+  task_g <- sl3::make_sl3_Task(
+    data = df_train,
+    covariates = setdiff(names(df_train), c('Y', 'A')),
+    outcome = 'A',
+    folds = folds
+  )
+  
+  sl_Q <- Lrnr_sl$new(
+    learners = lrnr_stack,
+    metalearner = ls_metalearner,
+    outcome_type = 'continuous'
+  )
+  
+  sl_g <- Lrnr_sl$new(
+    learners = lrnr_stack,
+    metalearner = lb_metalearner,
+    outcome_type = 'binomial'
+  )
+  
+  # fit Q and g
+  Q_fit <- sl_Q$train(task_Q)
+  g_fit <- sl_g$train(task_g)
+  
+  # preds Q and g
+  pred_Q_cf <- pred_Q_cf(df = df, Q_fit = Q_fit, folds = folds)
+  
+  task_g_pred <- sl3::make_sl3_Task(
+    data = df,
+    covariates = setdiff(names(df), c('Y', 'A')),
+    outcome = 'A'
+  )
+  pred_g <- g_fit$predict(task_g_pred)
+  
+  # bound g
+  pred_g[pred_g< 0.025] <- 0.025
+  pred_g[pred_g> 0.975] <- 0.975
+  
+  # mod.m <- gam(Y~s(X1) + s(X2) + ti(X1,X2)+s(X1,by=A) + s(X2,by=A) + ti(X1,X2,by=A),
+  #              family = gaussian(),data=df_train)
+  # mod.ps <- gam(A~s(X1)+s(X2)+ti(X1,X2),
+  #               family = binomial(),data=df_train) 
+  
+  # mod.m <- ranger::ranger(Y ~ .,data = df_train, num.trees =  200)
+  
+  tibble(
+    Y = df$Y, 
+    A = df$A,
+    pi_hat  = pred_g,
+    mu1_hat = pred_Q_cf$Qbar1W,
+    mu0_hat = pred_Q_cf$Qbar0W
+    # mu1_hat = predict(mod.m,mutate(df,A=1),type="response"),
+    # mu0_hat = predict(mod.m,mutate(df,A=0),type="response")
+  ) %>% return()
+}
+
+
+# helper function to predict Qbar1W and Qbar0W
+pred_Q_cf <- function(df, Q_fit, folds){
+  
+  # cf data
+  df1 <- df
+  df1$A <- 1
+  df0 <- df
+  df0$A <- 0
+  
+  # cf tasks
+  Q_task1 <- sl3::make_sl3_Task(
+    data = df1,
+    covariates = setdiff(names(df1), c('Y')),
+    outcome = 'Y',
+    folds = folds
+  )
+  
+  Q_task0 <- sl3::make_sl3_Task(
+    data = df0,
+    covariates = setdiff(names(df0), c('Y')),
+    outcome = 'Y',
+    folds = folds
+  )
+  
+  # cf Q
+  Qbar1W <- Q_fit$predict_fold(Q_task1,"validation")
+  Qbar0W <- Q_fit$predict_fold(Q_task0,"validation")
+  
+  return(list('Qbar1W' = Qbar1W,
+              'Qbar0W' = Qbar0W))
+}
+
+crossFitSL <- function(df,foldIDs,Nfolds=max(foldIDs)){
+  fold_list = lapply(1:Nfolds, function(i,foldIDs){which(foldIDs==i)},foldIDs = foldIDs)
+  
+  a <- sapply(fold_list,function(fold,df){
+    fitSL(df[fold,],df[-1*fold,]) 
+  },df=df,simplify = FALSE) %>% bind_rows()
+  
+  a <- a[order(unlist(fold_list)),] %>%
+    mutate(FoldID = foldIDs)
+  
+  return(a)
+}
+
+getFoldIDs <- function(N,Nfolds,shuffle=TRUE){
+  if (shuffle){
+    return(sample(rep_len(seq_len(Nfolds),length.out=N)))
+  } else{
+    return(rep_len(seq_len(Nfolds),length.out=N))
+  }
+}
---
+++ R/tmle.R
@@ -0,0 +1,100 @@
+#Function for computing ATE and VTE using TMLE
+#
+#data is a dataframe or tibble containing:
+#Y - outcome of interest
+#A - treatment indicator
+#pi_hat - estimate of the propensity score E(A|X)
+#mu1_hat - estimate of E(Y|A=1,X)
+#mu0_hat - estimate of E(Y|A=0,X)
+#
+#ab - (optional) Min and Max of range of Y. Defaults to min/max of (Y,mu0_hat,mu1_hat)
+
+TMLE_VTE <- function(data,ab=NULL){
+  ## Some params
+  max.it <- 2000 #maximum number of iterations in targetting step
+  eps <- 0.0001 #TMLE target step size
+  
+  a <- with(data,min(Y,mu1_hat,mu0_hat))
+  b <- with(data,max(Y,mu1_hat,mu0_hat)) - a
+  
+  if(!is.null(ab)){
+    if(ab[1]<=a & ab[2]>=(a+b)){
+      a <- ab[1]
+      b <- ab[2]-ab[1]
+    }else warning("ab values incorrect - using default method")
+  }
+
+  N = NROW(data)
+  A <- data$A
+  beta1 <- with(data,  1/pi_hat )
+  beta0 <- with(data,  1/(pi_hat-1) )
+  beta <- ifelse(A,beta1,beta0)
+  y <- (data$Y -a)/b
+
+  expitq1 <- qlogis((data$mu1_hat-a)/b)
+  expitq0 <- qlogis((data$mu0_hat-a)/b)
+  
+  retarget <- TRUE
+  it <- 1
+  while(retarget & it <= max.it){
+    q_1 <- plogis(expitq1)
+    q_0 <- plogis(expitq0)
+    cate <- q_1 - q_0
+    #q_m   <- q_0 + A*cate
+    #loss  <- -sum(y*log(q_m) + (1-y)*log(1-q_m))/N  
+    po <- beta*(y-q_0 - A*cate) + cate
+    
+    a0 <- sum(cate)/N
+    a1 <- sum(po)/N
+    b0 <- sum(cate^2)/N
+    b1 <- sum(po^2)/N
+    b2 <- sum(po*cate)/N
+    
+    ATE <- a0
+    VTE <- b0 - a0^2
+    pD1 <- a1 - a0
+    pD2 <- 2*(b2-b0+a0^2-a0*a1)
+    pD_norm <- sqrt(pD1^2 + pD2^2)
+    
+    Sig1 <- b1 - 2*a1*a0 + a0^2
+    Sig2 <- sum(((po-a0)^2 -(po-cate)^2 - VTE)^2  )/N 
+    
+    if( (N^2*pD1^2 >= Sig1) | (N^2*pD2^2 >= Sig2)){
+      expitq1 <- expitq1  + eps*beta1*(pD1+2*(cate-ATE)*pD2)/pD_norm
+      expitq0 <- expitq0  + eps*beta0*(pD1+2*(cate-ATE)*pD2)/pD_norm 
+      
+    }else{
+      retarget <- FALSE
+    }
+    it <- it+1
+    # print(paste0("it = ", it))
+    # print(N^2*pD2^2 )
+    # print(Sig2)
+
+  }
+  if(it>=max.it) warning("Max iterations reached in TMLE")
+  # print(N^2*pD1^2)
+  # print(Sig1)
+  # print(it)
+  
+  rootV <- ifelse(VTE>=0,sqrt(VTE),NA)
+  ss <- sqrt(Sig2/N)
+
+  coef <- c(ATE*b,
+            rootV*b,
+            VTE*b*b)
+  std.err <- c(sqrt(Sig1/N)*b,
+               ss*b/rootV,
+               ss*b*b)
+  names(coef) <- names(std.err) <- c("ATE","rootVTE","VTE")
+  names(cate) <- NULL
+  
+  out<- list(
+    coef = coef,
+    std.err = std.err,
+    CATE = cate*b
+  )
+  
+  return(out)
+}
+
