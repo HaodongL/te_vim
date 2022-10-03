15723b39d8190182de4e9291db4380447c3ed9bd
Haodong Li <haodong_li@berkeley.edu>
2022-10-02 20:22:21 -0700

    simulation setup

--- R/simu_theta_s.R
+++ R/simu_theta_s.R
@@ -14,12 +14,18 @@ library('doParallel')
 
 
 ###### set up the number of cores for parallel loop
-#ncore = 1
-
-#nCoresPerNode <- 1
-#nodeNames <-strsplit(Sys.getenv("SLURM_NODELIST"), ",")[[1]]
-#machines=rep(nodeNames, each = nCoresPerNode)
-#cl = makeCluster(machines, type = "SOCK")
+# parallel
+uname <- system("uname -a", intern = TRUE)
+os <- sub(" .*", "", uname)
+if(os=="Darwin"){
+  cpus_logical <- as.numeric(system("sysctl -n hw.logicalcpu", intern = TRUE))
+} else if(os=="Linux"){
+  cpus_logical <- system("lscpu | grep '^CPU(s)'", intern = TRUE)
+  cpus_logical <- as.numeric(gsub("^.*:[[:blank:]]*","", cpus_logical))
+} else {
+  stop("unsupported OS")
+}
+ncore <- floor(cpus_logical/2)
 
 
 ######  calculate the truth 
@@ -32,12 +38,11 @@ set.seed(1234)
 B <- 2 #rounds of simu
 N <- 5e2 #size of data
 
-#registerDoParallel(ncore)
+registerDoParallel(ncore)
 bootstrap_results <- run_all_simu(B = B, N = N, truth = 0.686)
 
 
 output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s',"theta_s_", N, "_", Sys.Date(),'.csv')
-
 write.csv(bootstrap_results, output_filename)
 
 
