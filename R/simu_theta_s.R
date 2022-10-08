e69124120d943f13d72e03567a13acb8b3513eb7
Haodong Li <haodong_li@berkeley.edu>
2022-10-08 10:19:14 -0700

    simu with gam

--- R/simu_theta_s.R
+++ R/simu_theta_s.R
@@ -43,10 +43,10 @@ ncore <- floor(cpus_logical/2)
 # 
 #   registerDoParallel(5)
 #   tic()
-#   bootstrap_results <- run_all_simu(B = B, N = N, cv = TRUE, dr = TRUE, lfm_linear = TRUE, truth = 0.686)
+#   bootstrap_results <- run_all_simu(B = B, N = N, cv = TRUE, dr = TRUE, truth = 0.686)
 #   toc()
 # 
-#   output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_earth_", N, "_", Sys.Date(),'.csv')
+#   output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_gam_", N, "_", Sys.Date(),'.csv')
 #   write.csv(bootstrap_results, output_filename)
 # }
 
@@ -54,15 +54,15 @@ ncore <- floor(cpus_logical/2)
 
 set.seed(1234)
 B <- 500 #rounds of simu
-N <- 2e4 #size of data
+N <- 1e4 #size of data
 
 registerDoParallel(5)
 tic()
-bootstrap_results <- run_all_simu(B = B, N = N, cv = F, dr = TRUE, max.it = 1e3, truth = 0.686)
+bootstrap_results <- run_all_simu(B = B, N = N, cv = FALSE, dr = TRUE, max.it = 1e3, truth = 0.686)
 toc()
 
 
-output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_gam_", N, "_", Sys.Date(),'.csv')
+output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_earth_nocv_", N, "_", Sys.Date(),'.csv')
 write.csv(bootstrap_results, output_filename)
 
 
