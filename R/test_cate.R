79ca3d277e90b4c1a1e670e439dbadc10f566f22
Haodong Li <haodong_li@berkeley.edu>
2022-10-18 20:26:27 -0700

    tmle v1

--- R/test_cate.R
+++ R/test_cate.R
@@ -5,6 +5,7 @@ source(paste0(repo_path, "R/sl3_config.R"))
 source(paste0(repo_path, "R/fit_para.R"))
 source(paste0(repo_path, "R/vim.R"))
 source(paste0(repo_path, "R/tmle_v0.R"))
+source(paste0(repo_path, "R/tmle_v1.R"))
 
 # source(paste0(repo_path, "R/sandbox/tmle_vim_linear.R"))
 library(tictoc)
@@ -49,7 +50,7 @@ res <- run_VIM_Theta(df = df,
                      cv = F,
                      dr = TRUE,
                      lfm_linear = FALSE, 
-                     max.it = 1e3, 
+                     max.it = 1e4, 
                      Q_bounds = c(0.001, 0.999), 
                      g_bounds = c(0.025, 0.975),
                      tau_bounds = c(-1+1e-3, 1-1e-3),
