386743bb242e66bbb26344a3c04f765042c35030
Haodong Li <haodong_li@berkeley.edu>
2022-10-21 10:19:01 -0700

    savio hal

--- R/simu_theta_s.R
+++ R/simu_theta_s.R
@@ -36,34 +36,34 @@ ncore <- floor(cpus_logical/2)
 
 ###### run simu
 
-# for (N in c(500, 1e3, 2e3,  3e3, 4e3, 5e3, 1e4, 2e4)){
-#   print(N)
-#   set.seed(1234)
-#   B <- 500 #rounds of simu
+for (N in c(500, 1e3, 2e3,  3e3, 4e3, 5e3, 1e4)){
+  print(N)
+  set.seed(1234)
+  B <- 500 #rounds of simu
+
+  registerDoParallel(10)
+  tic()
+  bootstrap_results <- run_all_simu(B = B, N = N, cv = F, dr = TRUE, max.it = 1e4, truth = 0.686)
+  toc()
+
+  output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_hal_nocv_", N, "_", Sys.Date(),'.csv')
+  write.csv(bootstrap_results, output_filename)
+}
+
+
+
+# set.seed(1234)
+# B <- 500 #rounds of simu
+# N <- 2e4 #size of data
 # 
-#   registerDoParallel(10)
-#   tic()
-#   bootstrap_results <- run_all_simu(B = B, N = N, cv = F, dr = TRUE, max.it = 1e4, truth = 0.686)
-#   toc()
-#   
-#   output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_earth_new2_", N, "_", Sys.Date(),'.csv')
-#   write.csv(bootstrap_results, output_filename)
-# }
-
-
-
-set.seed(1234)
-B <- 500 #rounds of simu
-N <- 2e4 #size of data
-
-registerDoParallel(5)
-tic()
-bootstrap_results <- run_all_simu(B = B, N = N, cv = F, dr = TRUE, max.it = 1e4, truth = 0.686)
-toc()
-
-
-output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_hal_", N, "_", Sys.Date(),'.csv')
-write.csv(bootstrap_results, output_filename)
+# registerDoParallel(5)
+# tic()
+# bootstrap_results <- run_all_simu(B = B, N = N, cv = F, dr = TRUE, max.it = 1e4, truth = 0.686)
+# toc()
+# 
+# 
+# output_filename <- paste0('~/Repo/te_vim/simu_res/theta_s/',"local_hal_nocv_", N, "_", Sys.Date(),'.csv')
+# write.csv(bootstrap_results, output_filename)
 
 
 
