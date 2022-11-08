169a1f10b68929b156237cb0a42b604683a314a8
Andrew Mertens <amertens@berkeley.edu>
2022-11-08 07:18:14 -0800

    rerun

--- R/data_explore.R
+++ R/data_explore.R
@@ -17,6 +17,38 @@ head(adhypo)
 
 #cleaned data
 final <- box_read("1048393668373")
-  
+dim(final)
+colnames(final)
 
+head(final)  
+
+library(caret)
+no_variance<-nzv(final)
+colnames(final)[no_variance]
+
+
+final#
+
+library(DataExplorer)
+
+
+final %>%
+  create_report(
+    output_format = html_document(toc = F,  theme = "yeti"),
+    output_file = paste("analysis_data_EDA", format(Sys.time(), "%Y-%m-%d %H:%M:%S %Z"), sep=" - "),
+    report_title = "EDA Report"
+    #y = "CVS"
+  )
+
+
+
+library(SmartEDA)
+
+# similarly, with dplyr syntax: df %>% ExpReport(...)
+ExpReport(
+  final,
+  #Target="cardio",
+  label=NULL,
+  op_file="Report.html",
+  op_dir=getwd())
 
