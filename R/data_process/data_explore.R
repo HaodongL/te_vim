39d785dca512e537f936927be379ee655c6c3eed
Haodong Li <haodong_li@berkeley.edu>
2023-02-10 07:57:00 -0800

    analysis data diab1

--- R/data_process/data_explore.R
+++ R/data_process/data_explore.R
@@ -1,22 +1,35 @@
+library(here)
+library(tidyverse)
+library(dplyr)
+library(mice)
 
+rm(list = ls())
+repo_path = "~/Repo/te_vim/"
+source(paste0(repo_path, "R/data_process/data_helper.R"))
 
-#ADCM 
-library(boxr)
-box_auth()
-#concommitant medication usage
-d <- box_read("946034255301")
-head(d)
-unique(d$CMTRT)
-
-
-#hypoglycemia data
-adhypo <- box_read("946034248101")
-head(adhypo)
+### ------------  Part 0. Import Dataset  ------------ ###
+if(here::here()=="C:/Users/andre/Documents/te_vim"){
+  library(boxr)
+  box_auth()
+  
+  
+}else{
+  # read in analysis data
+  ### ------------  Part 1. import data  ------------ ###
+  # outcome = 'diab'; t = 24
+  # df <- get_data(outcome, t, rm_baseIns=T)
+  df <- read_csv("~/Repo/te_vim/data/supp/df_analy1.csv")
+}
   
 
+# impute missingness
+nodes <- list(W = setdiff(names(df), c("Y", "A")),
+              A = 'A',
+              Y = 'Y')
 
-#cleaned data
-final <- box_read("1048393668373")
-  
+df <- process_missing(df, nodes)$data
+
+
+
 
 
